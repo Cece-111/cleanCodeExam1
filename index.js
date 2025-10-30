a6ecd988ab53af8683f2234664675535741fe176
Cece111 <cv.04@laposte.net>
2025-10-30 14:20:32 +0100

    creation du lancé de dés

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+export const roll5Dices = () => {
+    const listeDes = [];
+    for (let i = 0; i < 5; i++) {
+        listeDes.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return listeDes;
+};
+
 export const diceGame=(input)=> {
     return;
 };
--- index.test.js
+++ index.test.js
@@ -1,8 +1,15 @@
 import{describe,expect, it} from 'vitest';
 import {diceGame} from './index.js';
+import {roll5Dices} from './index.js';
 
 describe('diceGame',()=> {
-    it('should existe',()=> {
-        expect(true).toBe(true);
+    it('should return an array of 5 numbers between 1 and 6',()=> {
+        const result = roll5Dices();
+        expect(result).toBeInstanceOf(Array);
+        expect(result).toHaveLength(5);
+        result.forEach((value) => {
+            expect(value).toBeGreaterThan(0);
+            expect(value).toBeLessThan(7);
+        });
     });
 });
