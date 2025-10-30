371033dda18119a76d325ac28b967795752e4e6f
Cece111 <cv.04@laposte.net>
2025-10-30 14:13:03 +0100

    init fin

---
+++ index.test.js
@@ -0,0 +1,8 @@
+import{describe,expect, it} from 'vitest';
+import {diceGame} from './index.js';
+
+describe('diceGame',()=> {
+    it('should existe',()=> {
+        expect(true).toBe(true);
+    });
+});
